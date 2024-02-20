d42092dbca56bb60423b508ef0bb2b9906a18893
Thomas MICHEL <thomas.michel.thmi@gmail.com>
2024-02-20 18:18:02 +0100

    Fix some parameters and regulation and define target processing

--- src/models.py
+++ src/models.py
@@ -1,3 +1,14 @@
 from xgboost import XGBRegressor
 
-MyModel = XGBRegressor
+MyModel = XGBRegressor(
+    n_estimators=1000,
+    learning_rate=0.05,
+    n_jobs=-1,
+    random_state=0,
+    max_depth=3,
+    colsample_bytree=0.7,
+    subsample=0.7,
+    reg_alpha=0.5,
+    reg_lambda=0.5,
+    tree_method="gpu_hist",
+)
--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,28 @@
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
-def process_features(x_train,x_test):
+
+def process_features(x_train, x_test):
     scaler = StandardScaler()
     x_train_scaled = scaler.fit_transform(x_train)
     x_test_scaled = scaler.transform(x_test)
 
-    x_train_scaled = pd.DataFrame(x_train_scaled, columns=x_train.columns, index=x_train.index)
-    x_test_scaled = pd.DataFrame(x_test_scaled, columns=x_test.columns, index=x_test.index)
-    x_train_scaled.drop(columns=['predicted_spot_price'], inplace=True)
-    x_test_scaled.drop(columns=['predicted_spot_price'], inplace=True)
+    x_train_scaled = pd.DataFrame(
+        x_train_scaled, columns=x_train.columns, index=x_train.index
+    )
+    x_test_scaled = pd.DataFrame(
+        x_test_scaled, columns=x_test.columns, index=x_test.index
+    )
+    x_train_scaled.drop(columns=["predicted_spot_price"], inplace=True)
+    x_test_scaled.drop(columns=["predicted_spot_price"], inplace=True)
 
     x_train_scaled.ffill(inplace=True)
     x_test_scaled.ffill(inplace=True)
 
     return x_train_scaled, x_test_scaled
+
+
+def process_target(y_train, binarize=False):
+    if binarize:
+        y_train = (y_train >= 0).astype(int) * 2 - 1
+    return y_train
