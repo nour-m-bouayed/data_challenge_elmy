684f29cd3d81ab27507252f884530208102893e8
Thomas MICHEL <thomas.michel.thmi@gmail.com>
2024-03-15 09:57:22 +0100

    grid search results

--- src/models.py
+++ src/models.py
@@ -1,12 +1,13 @@
 from xgboost import XGBRegressor, XGBClassifier
 from darts.models import RNNModel, LinearRegressionModel
+from sklearn.ensemble import HistGradientBoostingClassifier
 
 def build_model(type='regression', lag_features=False):
     
     if type == 'regression':
         parent = XGBRegressor
     elif type == 'classification':
-        parent = XGBClassifier
+        parent = HistGradientBoostingClassifier
     elif type == 'forecaster':
         return build_forecaster()
 
@@ -17,7 +18,10 @@ def build_model(type='regression', lag_features=False):
             else:
                 return super().predict(X)
         
-    return MyModel(n_estimators=100, learning_rate=0.1, n_jobs=-1, alpha=0.1, reg_lambda=0.1, max_depth=3,)
+    if type == 'regression':
+        return MyModel(n_estimators=100, learning_rate=0.1, n_jobs=-1, alpha=0.1, reg_lambda=0.1, max_depth=3,)
+    elif type == 'classification':
+        return MyModel(learning_rate=0.001, max_iter=100, max_depth=20, max_leaf_nodes=127, min_samples_leaf=20, l2_regularization=0)
 
 def build_forecaster():
     return LinearRegressionModel( lags_future_covariates=(0,1), )
